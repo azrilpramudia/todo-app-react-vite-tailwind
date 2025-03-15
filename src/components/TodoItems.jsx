94c4e4b876f96cd691a71c38d9fe6e61e4f2c398
Azril Pramudia <azrilpramudia01@gmail.com>
2025-03-16 01:19:28 +0700

    feat add icon

---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import tick from "../assets/tick.png";
+// import not_tick from "../assets/no_tick.png";
+// import delete_icon from "../assets/delete.png";
+
+const TodoItems = () => {
+  return (
+    <div className="flex items-center my-3 gap-2">
+      <div>
+        <img src={tick} alt="" />
+        <p>Learn Coding</p>
+      </div>
+    </div>
+  );
+};
+
+export default TodoItems;
